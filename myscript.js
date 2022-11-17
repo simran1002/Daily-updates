f56b6202798f92758ae853adb2beb10d90d5cae1
simran1002 <simran2131056@akgec.ac.in>
2022-11-18 00:06:45 +0530

    updated

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-var
+var feet=
+document.getElementById('feet');
+var inch =
+document.getElementById('inch');
+
+feet.addEventListener('input',function(){
+    let f = this.value;
+    let i = f*12;
+    inch.value = i;
+});
+
+inch.addEventListener('input',function(){
+    let i = this.value;
+    let f = i/12;
+    if(!Number.isInteger(f)){
+        f=f/ToFixed(2);
+    };
+    feet.value =f;
+});
